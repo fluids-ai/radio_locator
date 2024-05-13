bb0bd01a2d7366a9368fec4948b81d996d021912
Hammad Usmani <ham.u@live.com>
2024-05-13 02:04:55 +0000

    Create boilerplate for the ETL Python class, references #1

---
+++ etl.py
@@ -0,0 +1,26 @@
+config = {
+    sources : {
+        'noaa' : 'https://www.weather.gov/nwr/station_search',
+        'amfm' : 'https://radio-locator.com/',
+        'sw' : 'https://short-wave.info/index.php'
+    }
+}
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def coordinate_results(lat, lon):
+    '''
+    Processes the latitude and longitude and returns the radio station results
+
+    Parameters
+    ----------
+    lat float
+        The decimal degrees with WGS 84 projection representing the latitude
+    lon float
+        The decimal degrees with WGS 84 projection representing the longitude
+
+    Returns
+    -------
+    '''
+    return None
